01fda89809b00b39d0b2db9dbb0b7a846b1012f8
William Oliveira Lima <williamolima@icloud.com>
2022-02-03 10:50:11 -0300

    Ajuste no construtor da data

--- Lembrete.kt
+++ Lembrete.kt
@@ -11,20 +11,6 @@ class Lembrete: Serializable {
         this.data = data
     }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_cadastrar)
-
-        val dia = intent.getIntExtra("dia", 0)
-        val mes = intent.getIntExtra("mes", 0)
-        val ano = intent.getIntExtra("ano",0)
-
-        tvData.text = dia.toString()+"/"+mes.toString()+"/"+ano.toString()
-
-        return tvData.text
-
-    }
-
     override fun toString(): String {
         return "${this.descricao} - ${this.data}"
     }
